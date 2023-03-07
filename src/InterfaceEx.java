13f3825d4b3aec9e4bfc251f6d8ce3bee8ad0cc2
name2965 <aha310510@gmail.com>
2023-03-07 22:06:45 +0900

    자바 공부 5일차(상속 공부완료)
    다음에는 자바 패키지 공부

---
+++ src/InterfaceEx.java
@@ -0,0 +1,40 @@
+interface PhoneInterface{
+    final int TIMEOUT = 10000;
+    void sendCall();
+    void receiveCall();
+    default void printLogo(){
+        System.out.println("** Phone **");
+    }
+}
+
+class Calc1{
+    public int calculate(int x,int y){
+        return x + y;
+    }
+}
+
+class SamsungPhone extends Calc1 implements PhoneInterface{
+    @Override
+    public void sendCall(){
+        System.out.println("띠리리리링");
+    }
+    @Override
+    public void receiveCall(){
+        System.out.println("전화가 왔습니다.");
+    }
+    public void flash(){
+        System.out.println("전화기에 불이 켜졌습니다.");
+    }
+    public void schedule(){
+        System.out.println("일정을 관리합니다.");
+    }
+}
+public class InterfaceEx {
+    public static void main(String[] args){
+        SamsungPhone phone = new SamsungPhone();
+        phone.printLogo();
+        phone.sendCall();
+        System.out.println("3과 5를 더하면 " + phone.calculate(3,5));
+        phone.schedule();
+    }
+}
